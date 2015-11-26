9ec41e1dae987da3937eefc37c1ef9416c3328f0
weahwww <weahwww@163.com>
2015-11-26 11:25:19 +0800

    Reactjs 倒计时按钮

---
+++ js/countdown/countdown.jsx
@@ -0,0 +1,54 @@
+/** @jsx React.DOM */
+var CountdownTimer = React.createClass({
+    getInitialState: function () {
+        return {
+            secondsRemaining: 10
+        };
+    },
+    
+    tick: function () {
+        this.setState({
+            secondsRemaining: this.state.secondsRemaining - 1
+        });
+        $("#clockbtn").text("倒计时("+ this.state.secondsRemaining + ")");
+        if (this.state.secondsRemaining <= 0) {
+            $("#clockbtn").text("验证");
+            $("#clockbtn").removeAttr('disabled');
+            clearInterval(this.interval);
+        }
+    },
+
+    onClickStart: function () {
+        this.setState({
+            secondsRemaining: this.props.secondsRemaining
+        });
+        
+        $("#clockbtn").text("倒计时("+ this.state.secondsRemaining + ")");
+        
+        this.interval = setInterval(this.tick, 1000);
+        $("#clockbtn").attr({'disabled':'disabled'});
+        
+    },
+    
+    componentDidMount: function () {
+        return {
+            secondsRemaining: 10
+        };
+    },
+    
+    componentWillUnmount: function () {
+        clearInterval(this.interval);
+    },
+    
+    render: function () {
+        return ( < button type = "button" id="clockbtn"
+            className = "btn btn-danger"
+            onClick = {this.onClickStart}>验证</button>
+        );
+    }
+});
+
+React.render(
+    <CountdownTimer secondsRemaining="10" />,
+    document.getElementById('output')
+);
